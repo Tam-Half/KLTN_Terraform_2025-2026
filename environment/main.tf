986407b753012069d875484ec10c675aeb2342a1
Tam Tran <tam.half28@gmail.com>
2026-05-05 17:49:08 +0700

    Iac ec2

---
+++ environment/main.tf
@@ -0,0 +1,22 @@
+## Keypair
+module "keypair" {
+    source            = "../module/keypair"
+    key_name          = "ec2_key_ssh"
+}
+
+## VPC
+module "vpc" {
+    source = "../module/vpc"
+    aws_name_vpc       = "kltn_vpc"
+    subnet_azs         = ["us-east-1a"]
+}
+
+## EC2
+module "ec2" {
+    source             = "../module/ec2"
+    aws_instance_type  = "t3.micro"
+    aws_ami            = "ami-091138d0f0d41ff90"
+    vpc_id             =  module.vpc.vpc_id
+    public_subnet      =  module.vpc.public_subnet_ids
+    keypair_name       =  module.keypair.key_name
+}
